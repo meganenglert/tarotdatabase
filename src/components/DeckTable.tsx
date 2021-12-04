5554808075606eca55010040aefeef0c4f3a557b
Megan Englert <megan.adare@live.com>
2021-12-04 13:41:21 -0500

    cards display

--- src/components/DeckTable.tsx
+++ src/components/DeckTable.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import '../App.css';
+import { Card, Col, Row } from 'react-bootstrap';
 import { Deck } from '../interfaces/deck';
 
 interface iDeckTable {
     decks: Deck[];
 }
 
-export function DeckTable({decks} : iDeckTable): JSX.Element {
+export function DeckTable({ decks }: iDeckTable): JSX.Element {
 
-    return <Row xs={1} md={2} className="g-4">
-            {decks.map((deck) => (
-                <Col>
-                    <p>There's a deck here!</p>
-                </Col>
-            ))}
-        </Row>;
+    return <Row xs={1} md={4} className="g-4">
+        {decks.map((deck) => (<Col>
+            <Card bg="light" text="dark">
+                <Card.Body>
+                    <Card.Title>{deck.title}</Card.Title>
+                    <Card.Text>
+                        {deck.artist}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Col>
+        ))}
+    </Row>;
 }
