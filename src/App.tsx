03e8c421211ec0c3e62ed94d183391cef0e2d096
Megan Englert <megan.adare@live.com>
2021-12-04 13:48:31 -0500

    filters is in its own column

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import DECKDATA from './assets/decks.json';
 import { Deck } from './interfaces/deck';
 import { DeckTable } from './components/DeckTable';
+import { Col, Row } from 'react-bootstrap';
+import { Filters } from './components/Filters';
 
 function App(): JSX.Element {
   const [decks, setDecks] = useState<Deck[]>(DECKDATA as Deck[]);
-    
+
   return (
     <div className="App">
       <h1>DeckFinder</h1>
-      <DeckTable decks={decks}></DeckTable>
+        <Row>
+          <Col xs={3}><Filters></Filters></Col>
+          <Col><DeckTable decks={decks}></DeckTable></Col>
+      </Row>
     </div>
   );
 }
---
+++ src/components/Filters.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../App.css';
+
+export function Filters(): JSX.Element {
+    return <div>
+        <h2>Filters</h2>
+    </div>;
+}
