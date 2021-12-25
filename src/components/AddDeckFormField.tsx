2895784a6774157551540dd0be3afeb3d7a5816d
Megan Englert <megan.adare@live.com>
2021-12-24 20:17:29 -0500

    new deck attributes data

--- src/components/AddDeckFormField.tsx
+++ src/components/AddDeckFormField.tsx
@@ -11,14 +11,23 @@ interface addDeckFormField {
 
 export function AddDeckFormField({ attributeKey, attribute, handleChange }: addDeckFormField): JSX.Element {
     return <div>
-        { attribute.type === fieldType.text &&
+        {attribute.type === fieldType.text &&
             <Form.Group as={Row} className="mb-3" controlId={attributeKey} onChange={handleChange}>
                 <Form.Label column sm="2">
                     {attribute.display}
                 </Form.Label>
                 <Col sm="10">
-                    <Form.Control type="text" placeholder={attribute.display} name={attributeKey}/>
+                    <Form.Control type="text" placeholder={attribute.display} name={attributeKey} />
                 </Col>
+            </Form.Group>
+        }
+        {attribute.type === fieldType.singleSelect &&
+            <Form.Group as={Row} className="mb-3" controlId={attributeKey} onChange={handleChange}>
+                <Form.Label column sm="2">
+                    {attribute.display}
+                </Form.Label>
+                <Col sm="10">
+                 </Col>
             </Form.Group>
         }
     </div>;
