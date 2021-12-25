914193b4f5306794f2f8820b2835708ac813bf91
Megan Englert <megan.adare@live.com>
2021-12-24 20:26:27 -0500

    attribute interface updated to match new data

--- src/interfaces/attribute.ts
+++ src/interfaces/attribute.ts
@@ -1,15 +1,17 @@
 export interface Attribute {
+    attribute: string;
     display: string;
     type: fieldType;
     tab: tab;
     default: string;
-    values?: string[];
+    values: string[];
 }
 
 export enum fieldType {
     text = "text",
     singleSelect = "single-select",
-    multiSelect = "multi-select"
+    multiSelect = "multi-select",
+    date = "date"
 }
 
 export enum tab {
