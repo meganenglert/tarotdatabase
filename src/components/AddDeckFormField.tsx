45e5ce4c14cb2751533b81821c1c0798a5b561a9
Megan Englert <megan.adare@live.com>
2021-12-26 23:37:12 -0500

    multiselect boxes added, currently do nothing - styling is bad but trust me it was worse

--- src/components/AddDeckFormField.tsx
+++ src/components/AddDeckFormField.tsx
@@ -1,36 +1,31 @@
 import { Col, Form, Row } from "react-bootstrap";
-import "../App.css";
 import { Attribute, fieldType } from "../interfaces/attribute";
 import ReactHookSelect from "react-hook-select";
 import "react-hook-select/dist/styles.scss";
+import "../App.css";
 
 interface addDeckFormField {
     attribute: Attribute,
     handleChange: (ev: React.ChangeEvent<HTMLFormElement>) => void
 }
 
-interface Item {
-    id: string,
-    name: string
-}
-
 export function AddDeckFormField({ attribute, handleChange }: addDeckFormField): JSX.Element {
 
     return <div>
         {attribute.type === fieldType.text &&
-            <Form.Group as={Row} className="mb-3" controlId={attribute.attribute} onChange={handleChange}>
+            <Form.Group as={Row} className="mb-3 align-items-center not-multi-row" controlId={attribute.attribute} onChange={handleChange}>
                 <Form.Label column sm="2">
-                    {attribute.display}
+                    <span className="attribute-label">{attribute.display}</span>
                 </Form.Label>
                 <Col sm="10">
-                    <Form.Control type="text" placeholder={"Ex. " + attribute.default} name={attribute.attribute} />
+                    <Form.Control type="text" placeholder={"e.g. " + attribute.default} name={attribute.attribute} />
                 </Col>
             </Form.Group>
         }
         {attribute.type === fieldType.singleSelect &&
-            <Form.Group as={Row} className="mb-3" controlId={attribute.attribute} onChange={handleChange}>
+            <Form.Group as={Row} className="mb-3 align-items-center not-multi-row" controlId={attribute.attribute} onChange={handleChange}>
                 <Form.Label column sm="2">
-                    {attribute.display}
+                    <span className="attribute-label">{attribute.display}</span>
                 </Form.Label>
                 <Col sm="10">
                     <Form.Select aria-label={attribute.attribute + "Select"}>
@@ -42,20 +37,23 @@ export function AddDeckFormField({ attribute, handleChange }: addDeckFormField):
             </Form.Group>
         }
         {attribute.type === fieldType.multiSelect &&
-            <ReactHookSelect
-                label="select"
-                enableMultiple={true}
-                enableSearch={false}
-                placeholder={"choose option"}
-                defaultValue={[]}
-                options={[
-                    { label: "Group A", value: "Group A", group: true },
-                    { label: "a", value: "a" },
-                    { label: "Group B", value: "Group B", group: true },
-                    { label: "b", value: "b" },
-                ]}
-                getValue={() => {}}
-            />
+            <Row className="align-items-center">
+                <Col sm="2">
+                    <p className="attribute-label">{attribute.display}</p>
+                </Col>
+                <Col sm="10" className="multiselect">
+                    <ReactHookSelect
+                        enableMultiple={true}
+                        enableSearch={false}
+                        placeholder={"Choose " + attribute.display}
+                        defaultValue={[]}
+                        options={attribute.values.map(value => {
+                            return { label: value, value: value }
+                        })}
+                        getValue={() => { }}
+                    />
+                </Col>
+            </Row>
         }
     </div>;
 }
